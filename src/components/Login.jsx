2cb2a6de7941b4bef96c21a9daec7f114184fa0d
lekan <oyeniranolalekan89@gmail.com>
2024-06-03 13:33:49 +0100

    made changes to the main.js

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,8 +16,8 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-semibold mb-4 text-center">Login</h2>
+      <div className="bg-white p-6 rounded shadow-xl w-full max-w-md">
+        <h2 className="text-3xl font-semibold mb-4 text-center">Login</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700">Email:</label>
@@ -43,7 +43,7 @@ const Login = () => {
           </div>
           <button 
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            className="w-full bg-blue-500 text-white py-2 rounded-xl hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
           >
             Login
           </button>
--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ const AppRouter = createBrowserRouter([
   {
     path: "/",
     element: <PrivateRoute />,
+    errorElement: <h4>404 not found</h4>,
     children: [
       {
         path: "/",
