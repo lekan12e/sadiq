10e4c5ddd3bef9baa0ea4df374f804e0153d8237
lekan <oyeniranolalekan89@gmail.com>
2024-11-28 13:41:11 +0100

    added a little bit of cu==============

--- src/devices/Iphone.jsx
+++ src/devices/Iphone.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Apple } from "../data/data";
 
 const Iphone = () => {
   const [data, setData] = useState(null);
@@ -8,12 +9,12 @@ const Iphone = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       fetch("http://localhost:3000/Iphone")
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           setData(data);
           setLoading(false);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error("Error fetching data:", error);
           setLoading(false);
         });
@@ -25,19 +26,24 @@ const Iphone = () => {
   console.log(data);
 
   return (
-    <div className='flex flex-wrap gap-4 max-w-[100%] overflow-x-hidden'>
+    <div className="flex flex-wrap gap-4 max-w-[100%] overflow-x-hidden">
       {loading ? (
         <div className="flex justify-center items-center w-full h-64">
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-red-500"></div>
         </div>
       ) : (
-        data && data.map(({ id, url, name }) => (
+        Apple &&
+        Apple.map(({ id, url, name }) => (
           <Link key={id} to={`/apple/${id}`}>
-            <div className='flex flex-col w-[120px] sm:w-[160px] items-start justify-center'>
-              <div className='w-full'>
-                <img className='max-w-full h-auto sm:max-w-[100px] sm:max-h-[100px]' src={url} alt={name} />
+            <div className="flex flex-col w-[120px] sm:w-[160px] items-start justify-center">
+              <div className="w-full">
+                <img
+                  className="max-w-full h-auto sm:max-w-[100px] sm:max-h-[100px]"
+                  src={url}
+                  alt={name}
+                />
               </div>
-              <h2 className='font-medium mt-2 text-center'>{name}</h2>
+              <h2 className="font-medium mt-2 text-center">{name}</h2>
             </div>
           </Link>
         ))
--- src/devices/Samsung.jsx
+++ src/devices/Samsung.jsx
@@ -1,19 +1,19 @@
-import React,{useState,useEffect} from 'react';
-import { JBLData } from '../data/data';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { SamsungData } from "../data/data";
+import { Link } from "react-router-dom";
 
 const Samsung = () => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const timer = setTimeout(() => {
       fetch("http://localhost:3000/SamsungData")
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           setData(data);
           setLoading(false);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error("Error fetching data:", error);
           setLoading(false);
         });
@@ -22,22 +22,28 @@ const Samsung = () => {
     return () => clearTimeout(timer); // Cleanup the timer
   }, []);
   return (
-    <div className='flex flex-wrap gap-4 max-w-[100%] overflow-x-hidden'>
+    <div className="flex flex-wrap gap-4 max-w-[100%] overflow-x-hidden">
       {loading ? (
         <div className="flex justify-center items-center w-full h-64">
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-red-500"></div>
         </div>
       ) : (
-      data && data.map(({ id, url, name }) => (
-        <Link key={id} to={`/samsung/${id}`}>
-          <div className='flex flex-col w-[120px] sm:w-[160px] items-start justify-center'>
-            <div className='w-full'>
-              <img className='max-w-full h-auto sm:max-w-[100px] sm:max-h-[100px]' src={url} alt={name} />
+        SamsungData &&
+        SamsungData.map(({ id, url, name }) => (
+          <Link key={id} to={`/samsung/${id}`}>
+            <div className="flex flex-col w-[120px] sm:w-[160px] items-start justify-center">
+              <div className="w-full">
+                <img
+                  className="max-w-full h-auto sm:max-w-[100px] sm:max-h-[100px]"
+                  src={url}
+                  alt={name}
+                />
+              </div>
+              <h2 className="font-medium mt-2 text-center">{name}</h2>
             </div>
-            <h2 className='font-medium mt-2 text-center'>{name}</h2>
-          </div>
-        </Link>
-      )))}
+          </Link>
+        ))
+      )}
     </div>
   );
 };
